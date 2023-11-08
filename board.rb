fbf8331b78d9d3a430797ba8acc34d82048ad9ee
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-08 21:54:45 +0100

    Add game over condition

--- board.rb
+++ board.rb
@@ -2,8 +2,15 @@
 
 # Game board
 class Board
+  attr_reader :winner
+
   def initialize
     @elements = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @chars = []
+  end
+
+  def add_char(player)
+    chars.push(player.character)
   end
 
   def display_board
@@ -22,13 +29,44 @@ class Board
     elements[position] = player.character
   end
 
+  def set_game_over
+    set_winner
+    @winner = 'none' if @winner.nil? && full?
+    nil
+  end
+
   private
 
   attr_accessor :elements
+  attr_reader :chars
+  attr_writer :winner
 
   WIN_POSITIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 5, 6]
   ].freeze
+
+  def full?
+    elements.all? { |element| element.is_a?(String) }
+  end
+
+  def won?(char)
+    won = false
+    moves = elements.each_index.select { |index| elements[index] == char }
+
+    WIN_POSITIONS.each do |win_position|
+      moves.combination(3).to_a.each do |move|
+        won = true if move == win_position
+      end
+    end
+    won
+  end
+
+  def set_winner
+    chars.each.with_index do |char, indx|
+      return @winner = indx if won?(char)
+    end
+    nil
+  end
 end
